7d489e75fdaded71d93979bc1e59d62737bba07d
Trenton Daniels <trentondaniels95@gmail.com>
2018-07-27 15:29:13 -0500

    created store class that contains item prices

--- LemonadeStand/Store.cs
+++ LemonadeStand/Store.cs
@@ -3,8 +3,46 @@ namespace LemonadeStand
 {
     public class Store
     {
+        // Members
+        private double cupPrice;
+        private double lemonPrice;
+        private double sugarPrice;
+        private double icePrice;
+
+        public double CupPrice
+        {
+            get
+            {
+                return cupPrice;
+            }
+        }
+        public double LemonPrice
+        {
+            get
+            {
+                return lemonPrice;
+            }
+        }
+        public double SugarPrice
+        {
+            get
+            {
+                return sugarPrice;
+            }
+        }
+        public double IcePrice
+        {
+            get
+            {
+                return icePrice;
+            }
+        }
+
+        // Constructors
         public Store()
         {
         }
+
+        // Methods
     }
 }
