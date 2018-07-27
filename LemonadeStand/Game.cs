eeb10dfcbcdf5c950b8e6bed2eeadc19f5bd1f56
Trenton Daniels <trentondaniels95@gmail.com>
2018-07-27 13:37:36 -0500

    created player class with functionality to get name

--- LemonadeStand/Game.cs
+++ LemonadeStand/Game.cs
@@ -3,9 +3,33 @@ namespace LemonadeStand
 {
     public class Game
     {
+        // Members
+        private Player player;
+        //private Customer customer; 
+        //private Day day;
+        //private Store store;
+
+        // Costructor
         public Game()
         {
-            Console.WriteLine("Hello World");
+            SetUpGame();
+            RunGame();
+        }
+
+        // Methods
+        private void SetUpGame() 
+        {
+            player = new Player();
+            DisplayWelcomeMessage();
+        }
+
+        private void RunGame()
+        {
+            
+        }
+        private void DisplayWelcomeMessage()
+        {
+            Console.WriteLine($"Welcome to Lemonade Stand {player.Name}!");
         }
     }
 }
---
+++ LemonadeStand/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace LemonadeStand
+{
+    public class Player
+    {
+        // Members
+        //private Inventory inventory;
+        private double amountOfMoney;
+        private string name;
+        private double priceOfLemonade;
+
+        public double PriceOfLemonade {
+            set 
+            {
+                priceOfLemonade = value;
+            }
+            get 
+            {
+                return priceOfLemonade;
+            }
+        }
+        public string Name {
+            get {
+                return name;
+            }
+        }
+
+        // Constructor
+        public Player()
+        {
+            name = GetName();
+        }
+
+        // Methods
+
+
+        public string GetName()
+        {
+            string playerName;
+
+            Console.WriteLine("What is your name?");
+            playerName = Console.ReadLine();
+
+            if (playerName.Length < 1)
+            {
+                Console.WriteLine("Invalid name. Please enter something next time!");
+                return GetName();
+            }
+            return playerName;
+
+        }
+    }
+}
